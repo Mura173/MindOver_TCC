889c441ef7025cc2151d4ea588ffb4c508bf5f21
Murilo Ângelo <murapb07@gmail.com>
2024-06-24 21:42:58 -0300

    Ground

---
+++ MindOver_TCC/Assets/Scripts/Slider.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+    public SliderJoint2D slider;
+    public JointMotor2D aux;
+
+    void Start()
+    {
+        aux = slider.motor;
+    }
+    void Update()
+    {
+        if(slider.limitState == JointLimitState2D.LowerLimit)
+        {
+            aux.motorSpeed = 1;
+            slider.motor = aux;
+        }
+
+        if (slider.limitState == JointLimitState2D.UpperLimit)
+        {
+            aux.motorSpeed = 1;
+            slider.motor = aux;
+        }
+    }
+}
