0cf927543831442be8528176763f33471a377bc0
Murilo Ângelo <murapb07@gmail.com>
2024-08-05 09:53:01 -0300

    Colecionavel Roxo

---
+++ MindOver_TCC/Assets/Scripts/Colecionavel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colecionavel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D item)
+    {
+        if (item.gameObject.CompareTag("Colecionavel"))
+        {
+            Destroy(item.gameObject);
+            Debug.Log("Pegou o item");
+        }
+    }
+}
