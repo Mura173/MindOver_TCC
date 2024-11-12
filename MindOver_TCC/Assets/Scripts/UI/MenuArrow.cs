05a91385370d2f9408795b0f0879ac7df5d10ab4
Murilo Ângelo <murapb07@gmail.com>
2024-11-11 22:21:49 -0300

    upd Menu

--- MindOver_TCC/Assets/Scripts/UI/MenuArrow.cs
+++ MindOver_TCC/Assets/Scripts/UI/MenuArrow.cs
@@ -7,44 +7,81 @@ using UnityEngine.SceneManagement;
 public class MenuArrow : MonoBehaviour
 {
     public Image arrow;
+    public GameObject buttonPanel;
 
     private bool isPlay = true;
+    private bool isInStart;
 
-    void Start()
+    public Animator panelButtonAnim, textAnim, bgAnim, arrowAnim;
+
+    public float xAnch, yAnch;
+
+    private void Start()
     {
-
+        isInStart = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveArrow();
+        StartMenu();
 
-        if (Input.GetKey(KeyCode.Z) && isPlay == true)
+        if (isInStart == false)
         {
-            SceneManager.LoadScene(1);
-        }
+            MoveArrow();
 
-        if (Input.GetKey(KeyCode.Z) && isPlay == false)
-        {
-            Application.Quit();
-        }
+            if (Input.GetKey(KeyCode.Z) && isPlay == true)
+            {
+                // SceneManager.LoadScene(1);
+            }
+
+            if (Input.GetKey(KeyCode.Z) && isPlay == false)
+            {
+                Application.Quit();
+            }
+        }       
     }
 
     void MoveArrow()
     {
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            arrow.rectTransform.anchoredPosition = new Vector2(40, -130);
-
+            arrow.rectTransform.anchoredPosition = new Vector2(-170, -930);
             isPlay = false;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            arrow.rectTransform.anchoredPosition = new Vector2(0, 0);
+            arrow.rectTransform.anchoredPosition = new Vector2(yAnch, xAnch);
 
             isPlay = true;
         }
+    }
+
+    void StartMenu()
+    {
+        if (isInStart == true)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                textAnim.SetBool("apertouZ", true);
+                bgAnim.SetBool("apertouZ", true);
+                isInStart = false;
+                StartCoroutine(SetPanelActive());
+            }          
+        }
+    }
+
+    IEnumerator SetPanelActive()
+    {       
+        yield return new WaitForSeconds(1);
+        buttonPanel.SetActive(true);
+        StartCoroutine(ArrowGo());
+    }
+
+    IEnumerator ArrowGo()
+    {
+        yield return new WaitForSeconds(0.5f);
+        arrowAnim.SetBool("go", true);
     }
 }
